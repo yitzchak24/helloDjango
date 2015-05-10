ae8b074164d7593808425ea4015bcb8b7aeef357
Isaac Lemus <lemusi24@yahoo.com>
2015-05-10 01:07:32 -0400

    mimic heroku getting started django proj

--- helloDjango/settings.py
+++ helloDjango/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'helloDjango.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': './test.db',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
